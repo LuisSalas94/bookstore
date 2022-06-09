253f48a95554eb33d30d6765c3499a473333aca1
Fernando <luisfernandosalasg@gmail.com>
2022-06-09 10:19:42 -0500

    Update repo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
-// import { Provider } from "react-redux";
 import Books from './components/Books';
 import Categories from './components/Categories';
-// import store from "./redux/configureStore";
 
 function App() {
   return (
