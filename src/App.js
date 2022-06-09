860d04f53444bc6470992b9d9faabec96535866e
Fernando <luisfernandosalasg@gmail.com>
2022-06-08 19:28:28 -0500

    Update repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+/* eslint-disable */
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import { Provider } from 'react-redux';
-import Books from './components/Books';
-import Categories from './components/Categories';
-import store from './redux/configureStore';
+//import { Provider } from "react-redux";
+import Books from "./components/Books";
+import Categories from "./components/Categories";
+//import store from "./redux/configureStore";
 
 function App() {
-  return (
-    <>
-      <ToastContainer />
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </Provider>
-    </>
-  );
+	return (
+		<>
+			<ToastContainer />
+			<Routes>
+				<Route path="/" element={<Books />} />
+				<Route path="/categories" element={<Categories />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,28 +1,41 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useEffect } from "react";
 // components
-import { useSelector } from 'react-redux';
-import Book from './Book';
-import Form from './Form';
+import { useSelector, useDispatch } from "react-redux";
+import { getBooks } from "../redux/books/books";
+import Book from "./Book";
+import Form from "./Form";
+
+const BookList = () => {
+	const books = useSelector((state) => state.books);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getBooks());
+	}, [dispatch]);
+
+	return (
+		<div className="books-container">
+			{books.map((item) => (
+				<Book
+					title={item.title}
+					author={item.author}
+					genre={item.genre}
+					id={item.id}
+					key={item.id}
+				/>
+			))}
+		</div>
+	);
+};
 
 const Books = () => {
-  const books = useSelector((state) => state.books);
-
-  return (
-    <>
-      <div className="books-container">
-        {books.map((item) => (
-          <Book
-            title={item.title}
-            author={item.author}
-            genre={item.genre}
-            id={item.id}
-            key={item.id}
-          />
-        ))}
-      </div>
-      <Form />
-    </>
-  );
+	return (
+		<>
+			<BookList />
+			<Form />
+		</>
+	);
 };
 
 export default Books;
